db10075d4488380e1403e291fcedab621ec6f02d
bikash1376 <bikash13763@gmail.com>
2024-01-15 02:49:20 +0530

    timeline data imported

---
+++ minimal-portfolio/src/components/data/timeline.js
@@ -0,0 +1,31 @@
+export default [
+    {
+        year: 'Today',
+        title:'Youtube Content Creator',
+        duration: '6 years',
+        details:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur laudantium culpa quidem corporis. Distinctio mollitia dicta excepturi illo expedita fugiat sint laborum necessitatibus ut. Numquam beatae veritatis dolorum fugiat consequatur? Beatae, mollitia."
+    },
+    {
+        year: 'Today',
+        title:'Solo Indie Developer',
+        duration: '4 years',
+        details:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur laudantium culpa quidem corporis. Distinctio mollitia dicta excepturi illo expedita fugiat sint laborum necessitatibus ut. Numquam beatae veritatis dolorum fugiat consequatur? Beatae, mollitia."
+    },
+    {
+        year: '2017',
+        title:'The Code Academy',
+        duration: '3 years',
+        details:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur laudantium culpa quidem corporis. Distinctio mollitia dicta excepturi illo expedita fugiat sint laborum necessitatibus ut. Numquam beatae veritatis dolorum fugiat consequatur? Beatae, mollitia."
+    }
+    ,
+    {
+        year: '2014',
+        title:'My Hustle',
+        duration: '2 years',
+        details:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur laudantium culpa quidem corporis. Distinctio mollitia dicta excepturi illo expedita fugiat sint laborum necessitatibus ut. Numquam beatae veritatis dolorum fugiat consequatur? Beatae, mollitia."
+    }
+]
