3e8f9fcfce564220495a65211e579615df741c23
bikash1376 <bikash13763@gmail.com>
2024-01-15 17:43:38 +0530

    completed all the sections

--- minimal-portfolio/src/components/Intro.jsx
+++ minimal-portfolio/src/components/Intro.jsx
@@ -5,14 +5,13 @@ const Intro = () => {
     <div className='flex items-center justify-center flex-col text-center pt-20 p-6'>
       <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold'>Forrest</h1>
       <p className='text-base md:text-xl mb-3 font-medium'>Software Engineer & Web Developer</p>
-      <p className='text-sm max-w-xl mb-6 font-bold'>This is my entire bio
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsum autem, delectus ullam similique deserunt repudiandae, eaque harum, est amet assumenda. Ab, consequatur magni asperiores perspiciatis sapiente adipisci laborum ducimus ex molestias blanditiis esse sed doloremque repellendus illum fugiat magnam, molestiae consectetur!
+      <p className='text-sm max-w-xl mb-6 font-bold'>
+Greetings! I'm Bikash Sahu, a 20-year-old Full Stack Developer with a fervor for AI and Machine Learning. My coding journey commenced in 2020, leading me to explore the realm of web development and contribute to open source projects. Proficient in the MERN stack, I've translated my passion into tangible results, crafting innovative websites and currently thriving in the freelance landscape.
       <br />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus similique voluptates veritatis rerum excepturi corporis?
-      {' '} <a href="https://www.github.com/bikash1376"
+      <br />
+      Explore my coding endeavors on {' '} <a href="https://www.github.com/bikash1376"
       target='_blank' className='text-cyan-600 hover:underline underline-offset-2 decoration-2 decoration-red-600' rel='noreferrer noopener'
-      >My Github</a> {' '} , aut hic odio iste commodi omnis ab qui ipsam reprehenderit ut, debitis dolorum ipsum possimus in?
-      </p>
+      >My Github</a> {' '} , where you'll find a collection of projects showcasing my dedication to creating impactful solutions and pushing the boundaries of what's possible in the digital world.</p>
     </div>
   )
 }
